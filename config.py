5498ce14b919cba4760fcdf7ff558729503dac2a
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-09-30 12:57:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 [pyrogram]
-api_id = xxxxx
-api_hash = xxxxxxxxxxxxxxxxxxxxxxxxx
-bot_token = xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+api_id = 6795023
+api_hash = 48eb04ae416967495ba9930f87d4f4da
+bot_token = 1928863257:AAEOTo7xuldOFkfuLs8tMiYKO49h_cgt1Wo
